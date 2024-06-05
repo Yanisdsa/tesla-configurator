3dab1720698c679ebce09991979281c1218a8f5b
ydesousa <yanis.desousaalves@soprasteria.com>
2024-06-05 14:58:50 +0200

    add database service

---
+++ src/app/core/services/tesla-database.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Signal, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ConfigCar, ModelCar } from '../model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TeslaDatabaseService {
+  private http = inject(HttpClient);
+
+  public getModels(): Signal<ModelCar[]> {
+    const modelCarObs$ = this.http.get<ModelCar[]>('/models');
+    return toSignal(modelCarObs$, { initialValue: [] });
+  }
+
+  public getOptions(modelCode: string): Signal<ConfigCar> {
+    const optionCarObs$ = this.http.get<ConfigCar>('/options/' + modelCode);
+    return toSignal(optionCarObs$, { initialValue: { configs: [], towHitch: false, yoke: false } });
+  }
+}
