20074b980eb2835478e4434d74851233e46d42b3
ydesousa <yanis.desousaalves@soprasteria.com>
2024-06-05 15:56:59 +0200

    add lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,20 @@
 import { Routes } from '@angular/router';
-import { CarModelChoiceComponent } from './components/car-model-choice/car-model-choice.component';
-import { CarOptionChoiceComponent } from './components/car-option-choice/car-option-choice.component';
-import { TotalCostComponent } from './components/total-cost/total-cost.component';
 import { firstStepCompleted, secondStepCompleted } from './core/guards';
 
 export const routes: Routes = [
-  { path: 'model', component: CarModelChoiceComponent },
-  { path: 'option', component: CarOptionChoiceComponent, canActivate: [firstStepCompleted]},
-  { path: 'total', component: TotalCostComponent, canActivate: [secondStepCompleted] },
-  { path: '', redirectTo: '/model', pathMatch: 'full' },
+  { 
+    path: 'model',
+    loadComponent: () => import('./components').then((c) => c.CarModelChoiceComponent),
+  },
+  { 
+    path: 'option',
+    loadComponent: () => import('./components').then((c) => c.CarOptionChoiceComponent),
+    canActivate: [firstStepCompleted]
+  },
+  { 
+    path: 'total',
+    loadComponent: () => import('./components').then((c) => c.TotalCostComponent),
+    canActivate: [secondStepCompleted]
+  },
+  { path: '', redirectTo: 'model', pathMatch: 'full' },
 ];
